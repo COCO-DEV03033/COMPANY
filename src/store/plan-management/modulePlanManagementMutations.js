a858c1255fee4ddc3e100952f0c801b49c200166
TopDeveloper <121497691+TopDeveloper215@users.noreply.github.com>
2024-06-10 02:11:45 +0900

    Update modulePlanManagementMutations.js

--- src/store/plan-management/modulePlanManagementMutations.js
+++ src/store/plan-management/modulePlanManagementMutations.js
@@ -1,28 +1,28 @@
 import { rearrangeData, updateArrays } from '@/utils/utils';
 
 export default {
-  GET_PLANS(state, { plans }) {
+  GET_PLANS (state, { plans }) {
     state.plans = plans
   },
-  CHANGE_EARNING(state, { changedata }) {
-    let changedatas = updateArrays (state.earnings, changedata)
+  CHANGE_EARNING (state, { changedata }) {
+    let changedatas = updateArrays(state.earnings, changedata)
     state.earnings = changedatas
     state.groupearnings = rearrangeData(changedatas, state.dates)
   },
   
-  GET_YEARMONTHS(state, { yearmonths }) {
+  GET_YEARMONTHS (state, { yearmonths }) {
     state.yearmonths = yearmonths
   },
 
-  UPDATE_YEARMONTH(state, { updatedata }) {
-    let changedatas = updateArrays (state.yearmonths, updatedata)
+  UPDATE_YEARMONTH (state, { updatedata }) {
+    let changedatas = updateArrays(state.yearmonths, updatedata)
     state.yearmonths = changedatas
   },
   
-  ADD_YEARMONTH(state, { adddata }) {
-    let data = state.yearmonths;
+  ADD_YEARMONTH (state, { adddata }) {
+    let data = state.yearmonths
     state.yearmonths.push(adddata)
     state.yearmonths = data
-  },
+  }
 
 }
