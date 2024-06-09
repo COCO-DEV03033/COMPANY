a24ee182ebb5478171d6c7831baf8a8d3b852e0c
TopDeveloper <121497691+TopDeveloper215@users.noreply.github.com>
2024-06-10 01:59:04 +0900

    add card delete function

--- src/views/main/planing/CreatePlan.vue
+++ src/views/main/planing/CreatePlan.vue
@@ -1,27 +1,11 @@
 <template>
     <div id="create-plan">
       <div class="vx-row" style="background-color: #fff; height: 100vh;"> 
-        <div class="vx-col w-full md:w-1/4" >
-          <vx-card style="border-top: 4px solid red; background-color: #f6f8fc;" >
-            <vs-list >
-              <div class="col text-center px-2 py-3 bg-light border rounded">
-                <p class="text-success font-bold text-3xl" >Team 1</p>
-              </div>
-              <draggable :list="list1" group="people" class="p-2 cursor-move">
-                <vs-list-item 
-                  v-for="(listItem, index) in list1" 
-                  :key="index" 
-                  :title="listItem.user.name" 
-                  :class="[getItemClass(index), 'base-item', { 'first-item': index === 0 }]"
-                >
-               
-                  <input type="text" placeholder="$" class="input_style" v-model="listItem.amount" />
-                  <vs-avatar slot="avatar" :text="listItem.user.avatar" />
-                
-                </vs-list-item>
-              </draggable>
-            </vs-list>
-          </vx-card>
+        <div class="vx-col w-full md:w-1/3" v-if="showCard1">
+          <DragCard :list_team1="list1" @delete-card="toggleCard('card1')" />
+        </div>
+        <div class="vx-col w-full md:w-1/3" v-if="showCard2">
+          <DragCard :list_team1="list1" @delete-card="toggleCard('card2')" />
         </div>
       </div>
     </div>
@@ -29,90 +13,69 @@
 <script>
 import draggable from 'vuedraggable'
 import { list2} from './data/userPlan.js'
+import DragCard from './component/DragCard.vue'
 import modulePlanManagement from '@/store/plan-management/modulePlanManagement.js'
 export default {  
   data () {
     return {
-      list2
+      list2,
+      showCard1: true,
+      showCard2: true
     }
   },
   components: {
-    draggable
+    draggable,
+    DragCard
   },
   computed: { 
     list1 () {
-      return this.$store.state.planManagement.plans;
+      return this.$store.state.planManagement.plans
     }
   },
-  methods : {
-    getItemClass (index) {
-      return {
-        'second-item': index === 1,
-        'third-item': index === 2
+  methods: {
+    addCard () {
+      if (!this.showCard1) {
+        this.showCard1 = true
+      } else if (!this.showCard2) {
+        this.showCard2 = true
+      }
+      // If both cards are already shown, you can handle it as needed
+    },
+    toggleCard (card) {
+      if (card === 'card1') {
+        this.showCard1 = false
+      } else if (card === 'card2') {
+        this.showCard2 = false
       }
     }
   },
+
+ 
   mounted () {
-    console.log('list1->',this.plans);
+    console.log('list1->', this.$store.state.planManagement.plans)
   },  
   beforeCreate () {
     if (!modulePlanManagement.isRegistered) {
-      this.$store.registerModule('planManagement', modulePlanManagement);
-      modulePlanManagement.isRegistered = true;
+      this.$store.registerModule('planManagement', modulePlanManagement)
+      modulePlanManagement.isRegistered = true
     }
 
     const payload = {
       year: 2024,
       month: 5,
       organization: 'all',
-      notify: this.$vs.notify,
-    };
+      notify: this.$vs.notify
+    }
     this.$store
       .dispatch('planManagement/fetchPlans', payload)
       .then((res) => {})
       .catch((err) => {
-        this.user_not_found = true;
-        return;
-        console.error(err);
-      });
+        this.user_not_found = true
+        return
+        console.error(err)
+      })
 
   }
 }
 </script>
-<style scoped>
-  .input_style{
-      width: 60%;
-      border: 1px solid rgb(187, 179, 179);
-      border-radius:  3px;
-      height: calc(1.5em + .75rem + 2px);
-      padding: .375rem .75rem;
-      font-size: 1rem;
-      
-  }
-  input:focus
-  {
-      outline: 1px solid #82b1eb;
-      border-color: green;
-  }
-  .first-item {
-    /* background-color: rgb(93, 93, 215); Add your specific styles here */
-    font-size: 1rem;
-    font-weight: bold;
-    color: red;
-  }
-  .base-item {
-    border: 2px solid #ffffff;
-    padding: 5px;
-    margin: 10px 0;
-    border-radius: 6px;
-    background-color: #ffffff;
-    justify-content: space-between;
-  }
-  .list-item-content {
-  /* display: flex;
-  align-items: center; */
-  justify-content: space-between; /* Ensures content is spread out */
-  /* width: 100%; */
-}
-</style>
   
--- src/views/main/planing/component/DragCard.vue
+++ src/views/main/planing/component/DragCard.vue
@@ -1,51 +1,80 @@
 <template>
   <div>
-    <div style="width: 25%;margin-left: 20px;margin-top: 30px;">
-      <div class="vx-row " >
-          <vs-list >
-            <draggable :list="list1" group="people" class="p-2 cursor-move" >
-              <vs-list-item 
-                v-for="(listItem, index) in list1" 
-                :key="index" 
-                :title="listItem.user.name"  
-                >
-                <input type="text" placeholder="$" class="input_style" v-model="listItem.amount" style="float: right;" />
-                <vs-avatar slot="avatar" :text="listItem.user.avatar" />
-              </vs-list-item>
-            </draggable>
-          </vs-list>
-      </div>
-    </div>
+    <vx-card style="border-top: 4px solid red; background-color: #f6f8fc;" >
+        <vs-list >
+          <button @click="deleteCard" style="float: right;">X</button>
+          <div class="col text-center px-2 py-3 bg-light border rounded">
+            <p class="text-success font-bold text-3xl" >Team 1</p>
+            
+          </div>
+          <draggable :list="list_team1" group="people" class="p-2 cursor-move">
+            <vs-list-item 
+              v-for="(listItem, index) in list_team1" 
+              :key="index" 
+              :title="listItem.user.name" 
+              :class="['base-item', { 'first-item': index === 0 }]"
+            >
+              <input type="text" placeholder="$" class="input_style" v-model="listItem.amount" />
+              <vs-avatar slot="avatar" :text="listItem.user.avatar" />
+            
+            </vs-list-item>
+          </draggable>
+        </vs-list>
+      </vx-card>
   </div>
 </template>
 <script>
-import { list1, list2} from '../data/userPlan.js'
+// import { list1, list2} from '../data/userPlan.js'
 import draggable from 'vuedraggable'
 export default {
   data () {
     return {
-      list1,
-      list2
+      
     }
   },
   components: {
     draggable
     
+  },
+  props :{
+    list_team1 :{
+      type: Object,
+      required : true
+    }
+  },
+  methods: {
+    deleteCard () {
+      this.$emit('delete-card')
+    }
   }
 }
 </script>
 <style scoped>
-  .input_style {
+  .input_style{
       width: 80px;
       border: 1px solid rgb(187, 179, 179);
       border-radius:  3px;
       height: calc(1.5em + .75rem + 2px);
       padding: .375rem .75rem;
       font-size: 1rem;
-      float: right;
+      
   }
   input:focus
   {
       outline: 1px solid #82b1eb;
+      border-color: green;
+  }
+  .first-item {
+    font-size: 1rem;
+    font-weight: bold;
+    color: red;
+  }
+  .base-item {
+    border: 2px solid #e7e4e4;
+    padding: 5px;
+    margin: 10px 0;
+    border-radius: 6px;
+    background-color: #ffffff;
+    
   }
 </style>
