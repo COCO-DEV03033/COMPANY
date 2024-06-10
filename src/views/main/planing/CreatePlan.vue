3b9040373848940135becaa52f77ff51417a22c3
TopDeveloper <121497691+TopDeveloper215@users.noreply.github.com>
2024-06-10 09:43:04 +0900

    update "Add", "Delete" component feature

--- src/views/main/planing/CreatePlan.vue
+++ src/views/main/planing/CreatePlan.vue
@@ -1,24 +1,47 @@
 <template>
     <div id="create-plan">
-      <div class="main_card"> 
+      <div class="main_card vx-row"> 
         <vx-col
           v-for="(card, index) in cards"
           :key="card.id"
-          :class="['w-full', 'md:w-1/3']"
+          :class="['w-full', 'md:w-1/3', 'xs:w-full', 'xl:w-1/3']"
         >
-          <DragCard :team="`Team ${card.id}`" :list_team1="card.list" @delete-card="removeCard(index)" />
+          <DragCard  
+            :team="`Team ${card.id}`" 
+            :list_team1="card.list" 
+            @delete-card="removeCard(index)" 
+            :color = "colorValue[index % colorValue.length]" 
+          />
+        </vx-col>
+        <vx-col>
+          <div @click="addCard" class="svg-button">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="26" height="26" viewBox="0 0 256 256" xml:space="preserve">
+
+            <defs>
+            </defs>
+            <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+              <path d="M 71.932 90 H 18.068 C 8.105 90 0 81.895 0 71.932 V 18.068 C 0 8.105 8.105 0 18.068 0 h 53.864 C 81.895 0 90 8.105 90 18.068 v 53.864 C 90 81.895 81.895 90 71.932 90 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(73,193,73); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+              <path d="M 45 70.454 c -2.761 0 -5 -2.238 -5 -5 V 24.545 c 0 -2.761 2.239 -5 5 -5 c 2.762 0 5 2.239 5 5 v 40.909 C 50 68.216 47.762 70.454 45 70.454 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+              <path d="M 65.454 50 H 24.545 c -2.761 0 -5 -2.238 -5 -5 c 0 -2.761 2.239 -5 5 -5 h 40.909 c 2.762 0 5 2.239 5 5 C 70.454 47.762 68.216 50 65.454 50 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+            </g>
+            </svg>
+          </div>
         </vx-col>
       </div>
     </div>
   </template>
 <script>
-import draggable from 'vuedraggable'
 import DragCard from './component/DragCard.vue'
 import modulePlanManagement from '@/store/plan-management/modulePlanManagement.js'
 export default {  
   data () {
     return {
+      colorValue : ['#db6a90', '#c875e8', '#8aa7fb', '#76d2b7', '#c974e3', '#00FFFF', '#78d2e2'],
       cards: [
+        {
+          id: '_other',
+          list: []
+        },
         {
           id: 1,
           list: [
@@ -57,14 +80,47 @@ export default {
           ]
         },
         {
-          id: '_other',
-          list: []
+          id: 3,
+          list: [
+            {
+              amount: 2000,
+              month: 6,
+              year: 2024,
+              user: {
+                userID: 'asd0303',
+                name: 'Pak',
+                dob: '1997/03/03',
+                avatar: '@/assets/images/portrait/small/avatar-s-26.jpg',
+                company: '3*9',
+                team: 2
+              }
+            }
+          ]
+        },
+        {
+          id: 4,
+          list: [
+            {
+              amount: 2000,
+              month: 6,
+              year: 2024,
+              user: {
+                userID: 'asd0303',
+                name: 'Pak',
+                dob: '1997/03/03',
+                avatar: '@/assets/images/portrait/small/avatar-s-26.jpg',
+                company: '3*9',
+                team: 2
+              }
+            }
+          ]
         }
+        
       ]
+      
     }
   },
   components: {
-    draggable,
     DragCard
   },
   computed: { 
@@ -73,6 +129,15 @@ export default {
     }
   },
   methods: {
+    addCard () {
+      const numericIds = this.cards.map(card => card.id).filter(id => !isNaN(id))
+      const newId = numericIds.length ? Math.max(...numericIds) + 1 : 1
+
+      this.cards.push({
+        id: newId,
+        list: []
+      })
+    },
     removeCard (index) {
       const othersIndex = this.cards.findIndex(card => card.id === '_other')
       if (othersIndex !== -1) {
@@ -111,13 +176,31 @@ export default {
 }
 </script>
 <style scoped>
-.main_card{
-  background-color: #fff;
-  height: 100vh;
-  overflow-x: scroll;
-  display: flex;
-  flex-direction: row;
-  gap: 30px;
-}
+  .main_card{
+    background-color: #fff;
+    height: 100vh;
+    overflow-x: scroll;
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    padding: 30px;
+    flex-wrap: nowrap !important;
+  }
+  .btn-glyphicon {
+      padding:8px;
+      background:#ffffff;
+      margin-right:4px;
+  }
+  .icon-btn {
+      padding: 1px 15px 3px 2px;
+      border-radius:50px;
+  }
+  .svg-button {
+    cursor: pointer;
+    transition: transform 0.3s ease; 
+  }
+  .svg-button:hover {
+    transform: scale(1.2);
+  }
 </style>
   
--- src/views/main/planing/component/DragCard.vue
+++ src/views/main/planing/component/DragCard.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <vx-card class="card_style">
-        <vs-list >
-          <button @click="deleteCard" class="float-right">X</button>
+    <vx-card :style="borderStyle" class="card_style">
+        <vs-list  >
+          <div @click="deleteCard"  class="float-right svg-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+            </svg>
+          </div>
           <div class="col text-center px-2 py-3 bg-light border rounded">
-            <p class="text-success font-bold text-3xl" >{{team}}</p>
+            <p class="text-success font-bold text-2xl" >{{team}}</p>
           </div>
           <draggable :list="list_team1" group="people" class="p-2 cursor-move">
             <vs-list-item 
               v-for="(listItem, index) in list_team1" 
               :key="index" 
               :title="listItem.user.name" 
-              :class="['base-item', { 'first-item': index === 0 }]"
+              :class="['base_item', { 'first_item': index === 0 }]"
             >
               <input type="text" placeholder="$" class="input_style" v-model="listItem.amount" />
               <vs-avatar slot="avatar" :text="listItem.user.avatar" />
-            
             </vs-list-item>
           </draggable>
-        </vs-list>
-      </vx-card>
+      </vs-list>
+    </vx-card>
   </div>
 </template>
 <script>
@@ -33,7 +37,6 @@ export default {
   },
   components: {
     draggable
-    
   },
   props :{
     list_team1 :{
@@ -43,45 +46,66 @@ export default {
     team:{
       type:String,
       required: true
+    },
+    color : {
+      type: Array,
+      required: true
     }
   },
   methods: {
     deleteCard () {
       this.$emit('delete-card')
     }
+  },
+  computed: {
+    borderStyle () {
+      return {
+        borderTop: `4px solid ${this.color}`,
+        backgroundColor: '#f6f8fc'
+      }
+    }
   }
 }
 </script>
 <style scoped>
   .input_style{
-      width: 80px;
+      width: 60px;
       border: 1px solid rgb(187, 179, 179);
       border-radius:  3px;
       height: calc(1.5em + .75rem + 2px);
       padding: .375rem .75rem;
       font-size: 1rem;
-      
   }
   input:focus
   {
       outline: 1px solid #82b1eb;
       border-color: green;
   }
-  .first-item {
+  .first_item {
     font-size: 1rem;
     font-weight: bold;
     color: red;
   }
-  .base-item {
+  .base_item {
     border: 2px solid #e7e4e4;
     padding: 5px;
     margin: 10px 0;
     border-radius: 6px;
     background-color: #ffffff;
-    
   }
-  .card_style{
-    border-top: 4px solid red;
-    background-color: #f6f8fc;
+  .svg-button {
+    cursor: pointer;
+    transition: transform 0.3s ease; 
+  }
+  .svg-button:hover {
+    transform: scale(1.2);
+  }
+</style>
+<style lang="scss">
+.card_style{
+    padding: 1px !important;
+    .vx-card__body{
+      padding: 5px !important
+    }
   }
 </style>
