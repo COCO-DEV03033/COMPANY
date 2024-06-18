9ea7e81e027a954a8c2e030cfe0614da2fcacaba
Best Dev <138656407+binaryking979@users.noreply.github.com>
2024-06-18 11:08:41 +0900

    projectview

--- src/store/project-management/moduleProjectManagementActions.js
+++ src/store/project-management/moduleProjectManagementActions.js
@@ -1,4 +1,5 @@
 import axios from '@/axios.js'
+import { resolve } from 'core-js/fn/promise'
 // import { resolve } from 'core-js/fn/promise'
 
 const API_URL = 'http://localhost:5050'
@@ -8,7 +9,7 @@ export default {
   fetchProjects ({ commit }) {
    
     return new Promise((resolve, reject) => {
-      axios.get(`${API_URL}/api/project/getProjects`)
+      axios.get(`${API_URL}/api/project`)
         .then((response) => {
           console.log(response)
           commit('SET_PROJECTS', {projects:response.data.data.projects})
@@ -20,9 +21,9 @@ export default {
 
   fetchProject ({commit}, payload) {
     return new Promise((resolve, reject) => {
-      axios.get(`${API_URL}/api/project/getProject/${payload}`)
+      axios.get(`${API_URL}/api/project/${payload}`)
         .then((response) => {
-          commit('SET_PROJECT', {project:response.data.existProject})
+          commit('SET_PROJECT', {project:response.data.data.projects})
           console.log('response->',response);
           resolve(response)
         })
@@ -30,11 +31,79 @@ export default {
     })
   },
 
-  removeProject ({ commit }, id) {
+  updateProject ({ commit }, payload) {
     return new Promise((resolve, reject) => {
-      axios.delete(`${API_URL}/api/data-list/products/${id}`)
-        .then((response) => {
-          commit('REMOVE_RECORD', id)
+      axios.post(`${API_URL}/api/project/${payload.id}`, payload)
+        .then((res) => {
+          // commit('UPDATE_YEARMONTH', { updatedata:payload })
+          if (res.data.status_code==0) {
+            this._vm.$vs.notify({
+              color: 'success',
+              title: 'Success',
+              text: res.data.message,
+            })
+          } else if(res.data.status_code==1) {
+            this._vm.$vs.notify({
+              color: 'warning',
+              title: 'Warning',
+              text: res.data.message,
+            });
+          } else {
+            console.log('error'); 
+          }
+        })
+        .catch((error) => { 
+          console.log(error)
+          reject(error) })
+    })
+  },
+  
+  addProject ({ commit }, payload) {
+    console.log(payload);
+    return new Promise((resolve, reject) => {
+      axios.post(`${API_URL}/api/project`, payload)
+        .then((res) => {
+          if (res.data.status_code == 0) {
+            this._vm.$vs.notify({
+              color: 'success',
+              title: 'Success',
+              text: res.data.message,
+            });
+            resolve(res.data.data)
+          } else if (res.data.status_code == 1) {
+            this._vm.$vs.notify({
+              color: 'warning',
+              title: 'Warning',
+              text: res.data.message,
+            });
+            resolve([])
+          }
+          resolve(response)
+        })
+        .catch((error) => { reject(error) })
+    })
+  },
+
+  deleteProject ({ commit }, payload) {
+    console.log('projectID', payload)
+    return new Promise((resolve, reject) => {
+      axios.delete(`${API_URL}/api/project/${payload.id}`, payload)
+        .then((res) => {
+          if (res.data.status_code == 0) {
+            this._vm.$vs.notify({
+              color: 'success',
+              title: 'Success',
+              text: res.data.message
+            });
+            resolve(res.data.data)
+          } else if (res.data.status_code == 1) {
+            this._vm.$vs.notify({
+              color: 'warning',
+              title: 'Warning',
+              text: res.data.message
+            });
+            resolve([])
+          }
           resolve(response)
         })
         .catch((error) => { reject(error) })
